3f34a928ad897222a9a0c4aee60db08c016f8cbd
Chris Burghart <burghart@ucar.edu>
2013-08-12 22:14:28 +0000

    Add conditional compilation for the archive_serializer_map kluges, which are only required for Boost 1.41.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/Archive_xmlrpc_c@8755 da89c675-a4d7-0310-8a15-9e7546503515

--- Archive_xmlrpc_c.cpp
+++ Archive_xmlrpc_c.cpp
@@ -4,17 +4,16 @@
  *  Created on: Jun 27, 2013
  *      Author: burghart
  */
-
-// Verify that we're compiling against Boost 1.42 or later. With Boost 1.41,
-// we can end up with link time errors like this:
-//    undefined reference to `boost::archive::detail::archive_serializer_map<Iarchive_xmlrpc_c>::find(boost::serialization::extended_type_info const&)'
-
 #define BOOST_ARCHIVE_SOURCE
 
+#include <boost/version.hpp>
 #include "Archive_xmlrpc_c.h"
 
-// we must explicitly instantiate some implementation for this type of stream
-#include <boost/archive/impl/archive_serializer_map.ipp>
-template class boost::archive::detail::common_oarchive<Oarchive_xmlrpc_c>;
-template class boost::archive::detail::common_iarchive<Iarchive_xmlrpc_c>;
-template class boost::archive::detail::archive_serializer_map<Iarchive_xmlrpc_c>;
+#if (BOOST_VERSION == 104100)
+   // For Boost 1.41, we must explicitly instantiate some implementation for
+   // this type of stream
+#  include <boost/archive/impl/archive_serializer_map.ipp>
+   template class boost::archive::detail::common_oarchive<Oarchive_xmlrpc_c>;
+   template class boost::archive::detail::common_iarchive<Iarchive_xmlrpc_c>;
+   template class boost::archive::detail::archive_serializer_map<Iarchive_xmlrpc_c>;
+#endif
